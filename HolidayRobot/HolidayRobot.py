8e8b052971070a9ae0a4e16ee89e1819747e4c6e
Adriano Godinho <adriano.daniel.goncalves.godinho@verizonconnect.com>
2025-11-17 17:13:41 +0000

    fix: removed unnecessary definition with super() call

--- HolidayRobot/HolidayRobot.py
+++ HolidayRobot/HolidayRobot.py
@@ -6,20 +6,12 @@ from holidayrobot_io.holidayrobot_io import HoRo_io
 from holidayrobot_etl.holidayrobot_etl import HoRo_etl
 
 class HolidayRobot(HoRo_io, HoRo_etl):
+    """class for the main HolidayRobot object. Here we have the configs and the data to work with."""
 
     def __init__(self, config):
         """initialize HolidayRobot"""
         self.HoRoIO = HoRo_io(config)
         self.config = config
         self.data = pd.DataFrame()  #creating here just for visibility, as etl functions create the 'data' variable
-
-    def read_data(self):
-        return super().read_data()
-    
-    def output_data(self):
-        return super().output_data()
-    
-    def perform_etl(self):
-        return super().perform_etl()
 
 
