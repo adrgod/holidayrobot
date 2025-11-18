326662a6fa32025e78fcc8dcc0adec289ff00c5a
Adriano Godinho <adriano.godinho@gmail.com>
2025-11-18 23:18:16 +0000

    chrore: some pylint fixes

--- holidayrobot_etl/holidayrobot_etl.py
+++ holidayrobot_etl/holidayrobot_etl.py
@@ -1,13 +1,13 @@
-
-#all functions to work on the data will be here
+"""class for the holiday robot ETL module"""
 import pandas as pd
 from support.performance import timer
 
 
 class HoRo_etl:
+    """class definition for HolidayRobot IO"""
 
     def __init__(self):
-        pass
+        self.data = pd.DataFrame()
 
 
     @timer
@@ -17,16 +17,16 @@ class HoRo_etl:
     @timer
     def _get_year_groups(self):
         """create new field with years grouped by 5"""
-        self.data['year_group'] = (self.data['Year'] // 5 * 5)
+        self.data['year_group'] = self.data['Year'] // 5 * 5
         self.data = self.data[['year_group', 'Sex', 'VALUE']].groupby(['year_group', 'Sex']).sum().reset_index()
         return self.data
 
     @timer
     def _rename_header(self):
+        """method to lower case the df header"""
         self.data.columns = self.data.columns.str.lower()
         return self.data
-    
-    
+
     @timer
     def perform_etl(self) -> pd.DataFrame:
         """perform all etl actions on data"""
