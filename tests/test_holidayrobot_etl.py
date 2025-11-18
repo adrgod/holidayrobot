6da89395702378a616e167d18d77911a72e1ba17
Adriano Godinho <adriano.godinho@gmail.com>
2025-11-18 00:20:53 +0000

    feat: adapted tests to new structure

---
+++ tests/test_holidayrobot_etl.py
@@ -0,0 +1,74 @@
+import pandas as pd
+import unittest
+from unittest.mock import patch
+from config.holidayrobot_config_class import HolidayRobotConfig
+from HolidayRobot.HolidayRobot import HolidayRobot
+
+class TestHolidayRobotFunctions(unittest.TestCase):
+
+    def setUp(self):
+        self.hr_config = HolidayRobotConfig()
+
+        self.HoRo_utest = HolidayRobot(self.hr_config)
+        self.HoRo_utest.data = pd.DataFrame({
+                'Year': [2010, 2011, 2015, 2018, 2023, 2024, 2025],
+                'Sex': ['Male', 'Male', 'Both', 'Female', 'Male', 'Female', 'Both'],
+                'VALUE': [100, 200, 300, 400, 500, 600, 700],
+                'Statistic Label': ['First Year', 'Third Year', 'First Year', 'No Year', 'Bla bla First Year', 'Bla bla Second Year', 'First Year']
+            })
+    
+    def test_filter_first_year(self):
+        """test filtering data to keep only data related to first year"""
+        expected_df = pd.DataFrame({
+                'Year': [2010, 2015, 2023, 2025],
+                'Sex': ['Male', 'Both', 'Male', 'Both'],
+                'VALUE': [100, 300, 500, 700],
+                'Statistic Label': ['First Year', 'First Year','Bla bla First Year', 'First Year']
+            })
+        result = self.HoRo_utest._fitler_first_year().reset_index(drop=True)
+        self.assertIsInstance(result, pd.DataFrame)
+        pd.testing.assert_frame_equal(result, expected_df)
+
+    def test_get_year_groups(self):
+        """test if data is grouped in 5 years time"""
+        expected_df = pd.DataFrame({
+                'year_group': [2010, 2015, 2015, 2020, 2020, 2025],
+                'Sex': ['Male', 'Both', 'Female', 'Female', 'Male', 'Both'],
+                'VALUE': [300, 300, 400, 600, 500, 700]
+            })
+        result = self.HoRo_utest._get_year_groups().reset_index(drop=True)
+        pd.testing.assert_frame_equal(result, expected_df)
+
+    def test_rename_header(self):
+        """test if header gets lower-cased"""
+        expected_df = pd.DataFrame({
+                'year': [2010, 2011, 2015, 2018, 2023, 2024, 2025],
+                'sex': ['Male', 'Male', 'Both', 'Female', 'Male', 'Female', 'Both'],
+                'value': [100, 200, 300, 400, 500, 600, 700],
+                'statistic label': ['First Year', 'Third Year', 'First Year', 'No Year', 'Bla bla First Year', 'Bla bla Second Year', 'First Year']
+            })
+        result = self.HoRo_utest._rename_header().reset_index(drop=True)
+        pd.testing.assert_frame_equal(result, expected_df)
+
+    @patch.object(HolidayRobot, '_fitler_first_year')
+    @patch.object(HolidayRobot, '_get_year_groups')
+    @patch.object(HolidayRobot, '_rename_header')
+    def test_perform_etl(self, mock_rename, mock_years, mock_filter):
+        """test if all etl functions get called"""
+        mock_filter.return_value = self.HoRo_utest.data
+        mock_years.return_value = self.HoRo_utest.data
+        mock_rename.return_value = self.HoRo_utest.data
+        
+        self.HoRo_utest.perform_etl()
+
+        mock_filter.assert_called_once()
+        mock_years.assert_called_once()
+        mock_rename.assert_called_once()
+
+    def tearDown(self):
+        del self.HoRo_utest
+        
+if __name__ == '__main__':
+    unittest.main()        
+
+    
---
+++ tests/test_holidayrobot_io.py
@@ -0,0 +1,53 @@
+import os
+import pandas as pd
+import unittest
+from unittest.mock import patch
+from config.holidayrobot_config import OUTPUT_PATH, OUTPUT_CSV, OUTPUT_PARQUET
+from config.holidayrobot_config_class import HolidayRobotConfig
+from HolidayRobot.HolidayRobot import HolidayRobot
+
+class TestHolidayRobotFunctions(unittest.TestCase):
+
+    def setUp(self):
+        self.hr_config = HolidayRobotConfig()
+
+        self.HoRo_utest = HolidayRobot(self.hr_config)
+        self.HoRo_utest.data = pd.DataFrame({
+                'Year': [2010, 2011, 2015, 2018, 2023, 2024, 2025],
+                'Sex': ['Male', 'Male', 'Both', 'Female', 'Male', 'Female', 'Both'],
+                'VALUE': [100, 200, 300, 400, 500, 600, 700],
+                'Statistic Label': ['First Year', 'Third Year', 'First Year', 'No Year', 'Bla bla First Year', 'Bla bla Second Year', 'First Year']
+            })
+    
+    def test_read_data(self):
+        """test reading the source data into a DataFrame"""
+        result = self.HoRo_utest.read_data().reset_index(drop=True)
+        self.assertIsInstance(result, pd.DataFrame)
+        self.assertGreater(len(result), 1)
+    
+    def test_offload_to_file_csv(self):
+        self.HoRo_utest._offload_to_file('csv')
+        self.assertTrue(os.path.isfile(f"{OUTPUT_PATH}/holidayrobot.csv"))
+
+    def test_offload_to_file_parquet(self):
+        self.HoRo_utest._offload_to_file('parquet')
+        self.assertTrue(os.path.isfile(f"{OUTPUT_PATH}/holidayrobot.parquet"))
+
+    @patch.object(HolidayRobot, '_offload_to_file')
+    @patch('os.path.isdir')
+    def test_output_data(self, mock_isdir, mock_offload):
+        self.HoRo_utest.output_data()
+        mock_isdir.return_value = True
+
+        if OUTPUT_CSV:
+                mock_offload.assert_any_call('csv')
+        if OUTPUT_PARQUET:
+            mock_offload.assert_any_call('parquet')
+
+    def tearDown(self):
+        pass
+        
+if __name__ == '__main__':
+    unittest.main()        
+
+    
