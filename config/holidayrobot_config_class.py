3f94e4f9480f96233aac653d192dace819052754
Adriano Godinho <adriano.godinho@gmail.com>
2025-11-18 00:19:32 +0000

    fix: added variable to config class

--- config/holidayrobot_config_class.py
+++ config/holidayrobot_config_class.py
@@ -1,6 +1,6 @@
 """config file to have all in one place for variables and params"""
 
-from config.holidayrobot_config import (PATH, OUTPUT_PATH, OUTPUT_CSV, OUTPUT_PARQUET, PRINT_PERFORMANCE)
+from config.holidayrobot_config import (PATH, OUTPUT_PATH, OUTPUT_CSV, OUTPUT_PARQUET, PRINT_PERFORMANCE, FORCE_CREATE_OUTPUT)
 
 class HolidayRobotConfig:
     """class to keep all configurations and parameters"""
@@ -11,3 +11,4 @@ class HolidayRobotConfig:
         self.output_csv = OUTPUT_CSV
         self.output_parquet = OUTPUT_PARQUET
         self.print_performance = PRINT_PERFORMANCE
+        self.force_create_output = FORCE_CREATE_OUTPUT
