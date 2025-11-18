0797c580c4b1d7d14293af4e73465e4385b1c2d7
Adriano Godinho <adriano.godinho@gmail.com>
2025-11-18 23:19:39 +0000

    chore: pylint fixes

--- tests/test_holidayrobot_etl.py
+++ tests/test_holidayrobot_etl.py
@@ -1,6 +1,6 @@
-import pandas as pd
 import unittest
 from unittest.mock import patch
+import pandas as pd
 from config.holidayrobot_config_class import HolidayRobotConfig
 from HolidayRobot.HolidayRobot import HolidayRobot
 
